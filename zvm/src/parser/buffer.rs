70df42eb419c942795b09d6587b30464a52f7870
zkrallah <muhammad.heshamyt@gmail.com>
2025-08-11 20:18:35 +0300

    fix: Handled out of bound panics when reading bytes from the byte array.

--- zvm/src/parser/buffer.rs
+++ zvm/src/parser/buffer.rs
@@ -11,35 +11,47 @@ impl Buffer {
         }
     }
 
-    pub fn read_u8(&mut self) -> u8 {
+    pub fn read_u8(&mut self) -> Option<u8> {
         let bytes = &self.bytes;
-        let result = bytes[self.offset];
 
-        self.offset += 1;
+        if self.offset < bytes.len() {
+            let result = bytes[self.offset];
 
-        result
+            self.offset += 1;
+            Some(result)
+        } else {
+            None
+        }
     }
 
-    pub fn read_u16(&mut self) -> u16 {
+    pub fn read_u16(&mut self) -> Option<u16> {
         let bytes = &self.bytes;
-        let result = u16::from_be_bytes([bytes[self.offset], bytes[self.offset + 1]]);
 
-        self.offset += 2;
+        if self.offset + 1 < bytes.len() {
+            let result = u16::from_be_bytes([bytes[self.offset], bytes[self.offset + 1]]);
 
-        result
+            self.offset += 2;
+            Some(result)
+        } else {
+            None
+        }
     }
 
-    pub fn read_u32(&mut self) -> u32 {
+    pub fn read_u32(&mut self) -> Option<u32> {
         let bytes = &self.bytes;
-        let result = u32::from_be_bytes([
-            bytes[self.offset],
-            bytes[self.offset + 1],
-            bytes[self.offset + 2],
-            bytes[self.offset + 3],
-        ]);
 
-        self.offset += 4;
+        if self.offset + 3 < bytes.len() {
+            let result = u32::from_be_bytes([
+                bytes[self.offset],
+                bytes[self.offset + 1],
+                bytes[self.offset + 2],
+                bytes[self.offset + 3],
+            ]);
 
-        result
+            self.offset += 4;
+            Some(result)
+        } else {
+            None
+        }
     }
 }
