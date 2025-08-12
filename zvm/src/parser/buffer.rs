c78efbc0148223b4bf1c1a5b672b529042df320c
zkrallah <muhammad.heshamyt@gmail.com>
2025-08-12 16:11:10 +0300

    fix: Off-by-one error fixed in reading the next  bytes.

--- zvm/src/parser/buffer.rs
+++ zvm/src/parser/buffer.rs
@@ -6,7 +6,7 @@ pub struct Buffer {
 }
 
 impl Buffer {
-    /// Creates a new `Buffer` from a vector of bytes, starting at offset 0.
+    /// Creates a new `Buffer` from a vector of bytes, starting at offset 0
     pub fn new(bytes: Vec<u8>) -> Self {
         Buffer {
             bytes: bytes,
@@ -14,7 +14,7 @@ impl Buffer {
         }
     }
 
-    /// Reads the next byte (`u8`) from the buffer.
+    /// Reads the next one byte (`u8`) from the buffer
     pub fn read_u8(&mut self) -> Option<u8> {
         let bytes = &self.bytes;
 
@@ -28,7 +28,7 @@ impl Buffer {
         }
     }
 
-    /// Reads the next byte (`u16`) from the buffer.
+    /// Reads the next two bytes (`u16`) from the buffer
     pub fn read_u16(&mut self) -> Option<u16> {
         let bytes = &self.bytes;
 
@@ -42,7 +42,7 @@ impl Buffer {
         }
     }
 
-    /// Reads the next byte (`u32`) from the buffer.
+    /// Reads the next four bytes (`u32`) from the buffer
     pub fn read_u32(&mut self) -> Option<u32> {
         let bytes = &self.bytes;
 
@@ -65,7 +65,7 @@ impl Buffer {
     pub fn read_un(&mut self, n: usize) -> Option<Vec<u8>> {
         let bytes = &self.bytes;
 
-        if self.offset + n < bytes.len() {
+        if self.offset + n - 1 < bytes.len() {
             let mut result = Vec::with_capacity(n);
 
             for _ in 0..n {
