00f01c3efa961c8b5a4b347125b01a0da554b147
zkrallah <muhammad.heshamyt@gmail.com>
2025-09-10 18:40:05 +0300

    docs: Generated simple rustdoc for the if_icmp<cond> instruction implmentation.

--- zvm/src/vm/instruction_exec.rs
+++ zvm/src/vm/instruction_exec.rs
@@ -45,21 +45,18 @@ impl InstructionExecutor {
             Opcode::Aload_2 => self.execute_aload_2(frame, pc),
             Opcode::Aload_3 => self.execute_aload_3(frame, pc),
             Opcode::Aaload => self.execute_aaload(frame, pc),
-
             Opcode::Ifeq => self.execute_ifeq(frame, pc),
             Opcode::Ifne => self.execute_ifne(frame, pc),
             Opcode::Iflt => self.execute_iflt(frame, pc),
             Opcode::Ifge => self.execute_ifge(frame, pc),
             Opcode::Ifgt => self.execute_ifgt(frame, pc),
             Opcode::Ifle => self.execute_ifle(frame, pc),
-
             Opcode::If_icmpeq => self.execute_if_icmpeq(frame, pc),
             Opcode::If_icmpne => self.execute_if_icmpne(frame, pc),
             Opcode::If_icmplt => self.execute_if_icmplt(frame, pc),
             Opcode::If_icmpge => self.execute_if_icmpge(frame, pc),
             Opcode::If_icmpgt => self.execute_if_icmpgt(frame, pc),
             Opcode::If_icmple => self.execute_if_icmple(frame, pc),
-
             Opcode::Return => self.execute_return(),
             Opcode::Getstatic => self.execute_getstatic(frame, class_file, runtime_data_area, pc),
             Opcode::Putstatic => self.execute_putstatic(frame, class_file, runtime_data_area, pc),
@@ -475,6 +472,7 @@ impl InstructionExecutor {
         Ok(true)
     }
 
+    /// Pop two integer values from the operand stack and check if they are equal
     fn execute_if_icmpeq(&self, frame: &mut Frame, pc: &mut usize) -> Result<bool, String> {
         if let Some(Value::Int(value2)) = frame.operand_stack.pop() {
             if let Some(Value::Int(value1)) = frame.operand_stack.pop() {
@@ -500,6 +498,7 @@ impl InstructionExecutor {
         Ok(true)
     }
 
+    /// Pop two integer values from the operand stack and check if they are not equal
     fn execute_if_icmpne(&self, frame: &mut Frame, pc: &mut usize) -> Result<bool, String> {
         if let Some(Value::Int(value2)) = frame.operand_stack.pop() {
             if let Some(Value::Int(value1)) = frame.operand_stack.pop() {
@@ -525,6 +524,8 @@ impl InstructionExecutor {
         Ok(true)
     }
 
+    /// Pop two integer values from the operand stack and check if the first is less than the
+    /// second
     fn execute_if_icmplt(&self, frame: &mut Frame, pc: &mut usize) -> Result<bool, String> {
         if let Some(Value::Int(value2)) = frame.operand_stack.pop() {
             if let Some(Value::Int(value1)) = frame.operand_stack.pop() {
@@ -550,6 +551,8 @@ impl InstructionExecutor {
         Ok(true)
     }
 
+    /// Pop two integer values from the operand stack and check if the first is greater than or equal
+    /// the second
     fn execute_if_icmpge(&self, frame: &mut Frame, pc: &mut usize) -> Result<bool, String> {
         if let Some(Value::Int(value2)) = frame.operand_stack.pop() {
             if let Some(Value::Int(value1)) = frame.operand_stack.pop() {
@@ -575,6 +578,7 @@ impl InstructionExecutor {
         Ok(true)
     }
 
+    /// Pop two integer values from the operand stack and check if the first is greater than the second
     fn execute_if_icmpgt(&self, frame: &mut Frame, pc: &mut usize) -> Result<bool, String> {
         if let Some(Value::Int(value2)) = frame.operand_stack.pop() {
             if let Some(Value::Int(value1)) = frame.operand_stack.pop() {
@@ -600,6 +604,8 @@ impl InstructionExecutor {
         Ok(true)
     }
 
+    /// Pop two integer values from the operand stack and check if the first is less than or equal
+    /// the second
     fn execute_if_icmple(&self, frame: &mut Frame, pc: &mut usize) -> Result<bool, String> {
         if let Some(Value::Int(value2)) = frame.operand_stack.pop() {
             if let Some(Value::Int(value1)) = frame.operand_stack.pop() {
