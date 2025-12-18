b4d8f25905047bf62e69e41fd75bcb314953c417
zkrallah <muhammad.heshamyt@gmail.com>
2025-12-18 17:42:27 +0200

    feat: Introduced a simple bytecode generator that uses both ASM and bytebuddy to generate bytecode class files.

---
+++ zvm/src/samples/bytez-generator/src/main/java/com/zkrallah/bytez/App.java
@@ -0,0 +1,10 @@
+package com.zkrallah.bytez;
+
+public class App {
+    public static void main(String[] args)
+            throws Exception {
+        GenerateHelloASM.generate();
+        GenerateHelloByteBuddy.generate();
+        GenerateSwap.generate();
+    }
+}
