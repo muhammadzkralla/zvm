477d8106c9776cd91bd72b4dc73de79babe41688
zkrallah <muhammad.heshamyt@gmail.com>
2026-09-14 13:56:32 +0300

    docs: Initial fourth blog about execution.

--- zvm/src/samples/sample1/Main.java
+++ zvm/src/samples/sample1/Main.java
@@ -2,21 +2,20 @@ public class Main implements IMain {
 
     private static final String str = "lol";
 
+    public static void main(String[] args) {
+        foo(32, 501);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+
     public static void foo(int num1, int num2) {
         System.out.println(num1);
         System.out.println(num2);
-        String s = bar();
-        System.out.println(s);
+        bar();
     }
 
     public static void bar() {
         System.out.println("Wait... WTF IT WORKED????? jhafkjhlskjhakjg");
-    }
-
-    public static void main(String[] args) {
-        foo(404, 501);
-        System.out.println(args[0]);
-        System.out.println(args[1]);
     }
 
     @Override
